2bdef60436578f93384b23a09b1b12bf986fb416
김경식 <envy7937@gmail.com>
2021-02-26 11:37:09 +0900

    Fix 불필요한 페이지 삭제

--- pages/index.vue
+++ pages/index.vue
@@ -1,97 +1,555 @@
 <template>
 	<div class='inner'>
-		<!-- tab -->
-		<!-- v-img : vuetify의 이미 만들어진 컴포넌트 -->
-		<v-img :src='(activeTab === 0 ? event1TabImg : event2TabImg)'>
-			<!-- v-bind를 이용한 동적인 prop 전달 -->
-			<!-- @{이벤트명} 을 이용하여 자식 컴포넌트에서 emit한 이벤트에 대한 처리를 설정 -->
-			<old-tab
-				:items="tabItems"
-				:active="activeTab"
-				:active2="activeTab2"
-				@input='changeActiveTab'
-			>
-			</old-tab>
-		</v-img>
+		<!-- tabs -->
+		<tabs :items='tabItems'
+			  :active='activeTab'
+			  evClass='main-tab'
+		>
+			<!--
+			슬롯 속성(slotProps)
+			부모 컴포넌트에서 자식 컴포넌트의 슬롯에
+			바인딩된 데이터에 접근하기 위하여 사용
+			-->
+			<template #title='{item, active}'>
+				<!-- tab -->
+				<tab :item='item' :active='active'
+					 @input='changeMainTab'
+				></tab>
+			</template>
+		</tabs>
 
-		<!-- 본문 -->
-		<div v-show='activeTab === 0'>
-			<v-img :src='event1Img1'></v-img>
-			<v-img :src='event1Img2'></v-img>
-			<v-img :src='event1Img3'>
-				<div class='banner-img'>
-					<img src='~/assets/images_aj/event1/ico/ev1_ico_banner.png'>
-				</div>
-			</v-img>
-			<v-img :src='event1Img4'></v-img>
-			<v-img :src='event1Img5'></v-img>
-			<v-img :src='event1FootImg'></v-img>
-		</div>
-		<div v-show='activeTab === 1'>
-			<v-img :src='event2Img1'></v-img>
-			<v-img :src='event2Img2'></v-img>
-			<v-img :src='event2Img3'></v-img>
-			<v-img :src='event2Img33'>
-				<btn btnType='more-btn' >인증점 더보기</btn>
-			</v-img>
-			<v-img :src='event2Img4'>
-				<div class='banner-img'>
-					<img src='~/assets/images_aj/event2/ico/ev2_ico_banner.png'>
-				</div>
-			</v-img>
-			<v-img :src='event2FootImg'></v-img>
-		</div>
+		<!-- tab-items -->
+		<tab-items :items='tabItems' :active='activeTab'>
+			<template #event1='{item}'>
+				<!-- tab-item -->
+				<tab-item :item='item' :active='activeTab'>
+					<!-- section1_1 -->
+					<event-section :src="require('~/assets/images_aj/event1/ev1_kv.jpg')">
+					</event-section>
+
+					<!-- section1_2 -->
+					<event-section :src="require('~/assets/images_aj/event1/ev1_step.jpg')">
+					</event-section>
+
+					<!-- section1_3 -->
+					<event-section :src="require('~/assets/images_aj/event1/ev1_tmp_banner.jpg')">
+						<img :src="require('~/assets/images_aj/event1/ico/ev1_ico_banner.png')"
+							 class='ico-banner' />
+						<btn class='btn-box'
+							 btn-type='line-type'
+							 colorType='color-yellow'
+							 width='235px'
+							 left='420px'
+							 bottom='16px'
+						>내 주변 한우 인증점 보러가기
+						</btn>
+					</event-section>
+
+					<!-- section1_4 -->
+					<event-section :src="require('~/assets/images_aj/event1/ev1_prize.jpg')">
+					</event-section>
+
+					<!-- section1_5 -->
+					<event-section :src="require('~/assets/images_aj/event1/ev1_join.jpg')">
+						<btn class='btn-box bg-type'
+							 btn-type='btn-entry'
+							 width='360px'
+							 left='330px'
+							 bottom='100px'
+							 @click.native='evStep01On = true'
+						>응모하기
+						</btn>
+
+						<!-- event1 응모 step1 modal-->
+						<event-modal v-if='evStep01On' @close='closeModal'>
+							<template #title>
+								한우먹고!<i class='point'>한우인증 Go~!</i>
+							</template>
+							<template slot='sub-title'>
+								수험생과 일반 참여 방법이 다릅니다!<br>참여 유형을 선택해주세요.
+							</template>
+							<template #content>
+								<ul class='select'>
+									<li @click="selectApplyType('examinee')">수험생으로 참여할꺼에요!</li>
+									<li @click="selectApplyType('public')">일반으로 참여할꺼에요!</li>
+								</ul>
+							</template>
+						</event-modal>
+
+						<!-- event1 응모 step2 modal-->
+						<event-modal v-if='evStep02On' @close='closeModal'>
+							<template #title>
+								한우먹고!<i class='point'>한우인증 Go~!</i>
+							</template>
+							<template slot='sub-title'>
+								경품 발송시 연락드릴 연락처를 입력해주세요.
+								<i class='point'>한우 인증점 로고와
+									<span>{{ (event1Data.type == 'examinee') ? '수험표가' : '영수증이' }}</span>
+									나온 이미지를 업로드 후,<br>
+									개인정보를 남겨주셔야 이벤트 참여가 완료됩니다.
+								</i>
+							</template>
+							<template #content>
+								<form>
+									<div class='input-area'>
+										<div class='flex-box'>
+											<event-input
+												id='cert_file_name'
+												placeholder='이미지는 한장만 업로드 가능합니다.'
+												styleType='m-file'
+												:value='filename'
+												disabled='true'
+											/>
+											<event-input input-type='file'
+														 id='cert_file'
+														 name='cert_file'
+														 @input='setUploadFile'
+											/>
+										</div>
+										<span class='info'>&#42; Jpg, Jpeg, Png 파일만 업로드 가능합니다.</span>
+									</div>
+
+									<event-input
+										input-type='text'
+										id='name'
+										name='name'
+										v-model='event1Data.name'
+										placeholder='이름' />
+									<event-input
+										input-type='text'
+										id='phone'
+										name='phone'
+										v-model='event1Data.phone'
+										placeholder='전화번호' />
+									<event-input
+										input-type='text'
+										id='email'
+										name='email'
+										v-model='event1Data.email'
+										placeholder='이메일' />
+
+									<aggrement padding-left='40px'>
+										<template #checkbox>
+											<event-input
+												input-type='checkbox'
+												name='aggr1'
+												id='aggr1'
+												v-model='event1Data.aggrchk1'>
+												<i class='point'>(필수)</i> 개인정보 활용 동의
+											</event-input>
+										</template>
+										<template #detail_btn_text>자세히보기</template>
+										<template #content>
+											<p>개인정보 수집 동의</p>
+											<p>
+												1. 수집 항목 : [필수] 이름, 전화번호, 이메일<br />
+												2. 수집 및 이용 목적 : 이벤트 참여<br />
+												3. 업무 위탁 제공처 : 한우자조금관리위원회 위탁 협력업체<br />
+												4. 보유 및 이용기간 : 해당 목적 달성 후 폐기
+											</p>
+										</template>
+									</aggrement>
+
+									<aggrement padding-left='20px'>
+										<template #checkbox>
+											<event-input
+												input-type='checkbox'
+												name='aggr2'
+												id='aggr2'
+												v-model='event1Data.aggrchk2'>
+												(선택) 마케팅 정보 수신 동의
+											</event-input>
+										</template>
+										<template #detail_btn_text>자세히보기</template>
+										<template #content>
+											<p>마케팅 정보 수신 동의</p>
+											<p>
+												1. 수집 항목 : 이벤트 참여자 이름, 전화번호, 이메일<br />
+												2. 수집 및 이용 목적 : 한우자조금관리위원회에서 진행하는 이벤트 및
+												프로모션 안내<br />
+												3. 업무 위탁 제공처 : 한우자조금관리위원회 위탁협력업체<br />
+												4. 보유 및 이용기간 : 위탁 목적 및 계약 종료시까지<br />
+												5. 동의 철회 방법 : 개인정보 보호 관리자 및 담당자에게 서면, 전화
+												또는 이메일 연락
+											</p>
+										</template>
+									</aggrement>
+
+									<btn class='bg-type confirm-btn'
+										 btn-type='btn-confirm'
+										 width='280px'
+										 @click.native='event01Submit'
+									>확 인
+									</btn>
+								</form>
+							</template>
+						</event-modal>
+
+						<!-- event1 응모 step3 modal-->
+						<event-modal v-if='evStep03On' @close='closeModal'>
+							<template #title>
+								한우먹고!<i class='point'>한우인증 Go~!</i>
+							</template>
+							<template slot='sub-title'>
+								이벤트 참여가 완료되었습니다.
+							</template>
+							<template #content>
+								<btn class='bg-type confirm-btn'
+									 btn-type='btn-confirm'
+									 width='280px'
+									 @click.native='closeModal'
+								>확 인
+								</btn>
+							</template>
+						</event-modal>
+					</event-section>
+				</tab-item>
+			</template>
+
+			<template #event2='{item}'>
+				<!-- tab-item -->
+				<tab-item :item='item' :active='activeTab'>
+					<!-- section2_1 -->
+					<event-section :src="require('~/assets/images_aj/event2/ev2_kv.jpg')">
+					</event-section>
+
+					<!-- section2_2 -->
+					<event-section :src="require('~/assets/images_aj/event2/ev2_step.jpg')">
+					</event-section>
+
+					<!-- section2_3 -->
+					<event-section class='ev2-bg-color'>
+						<tabs :items='areaTabItem'
+							  :active='areaActiveTab'
+							  class='list-wrap'
+							  evClass='area-tab'
+						>
+							<template #title='{item, active}'>
+								<!-- tab -->
+								<tab :item='item' :active='active'
+									 @input='changeAreaTab'
+								></tab>
+							</template>
+						</tabs>
+
+
+						<tab-item :items='areaTabItem'
+								  :active='areaActiveTab'
+								  class='list-wrap'>
+							<!-- 전국 -->
+							<template #area1_1>
+								<grid :items='storeList' />
+
+								<btn btn-type='btn-more'
+									 class='bg-type'
+									 width='360px'
+									 height='64px'
+								>인증점 더보기
+								</btn>
+							</template>
+							<!-- 수도권 -->
+							<template #area2_1>
+								<grid :items='storeList'
+									  max-row='2'
+									  max-col='3' />
+							</template>
+							<!-- 충청도 -->
+							<template #area3_1>
+								area3
+
+								<btn btn-type='btn-more'
+									 class='bg-type'
+									 width='360px'
+									 height='64px'
+								>인증점 더보기
+								</btn>
+							</template>
+							<!-- 전라도 -->
+							<template #area4_1>
+								area4
+
+								<btn btn-type='btn-more'
+									 class='bg-type'
+									 width='360px'
+									 height='64px'
+								>인증점 더보기
+								</btn>
+							</template>
+							<!-- 경상도 -->
+							<template #area5_1>
+								area5
+
+								<btn btn-type='btn-more'
+									 class='bg-type'
+									 width='360px'
+									 height='64px'
+								>인증점 더보기
+								</btn>
+							</template>
+							<!-- 강원도 -->
+							<template #area6_1>
+								area6
+
+								<btn btn-type='btn-more'
+									 class='bg-type'
+									 width='360px'
+									 height='64px'
+								>인증점 더보기
+								</btn>
+							</template>
+							<!-- 제주도 -->
+							<template #area7_1>
+								area7
+
+								<btn btn-type='btn-more'
+									 class='bg-type'
+									 width='360px'
+									 height='64px'
+								>인증점 더보기
+								</btn>
+							</template>
+						</tab-item>
+					</event-section>
+
+					<!-- section2_4 -->
+					<event-section :src="require('~/assets/images_aj/event2/ev2_tmp_banner.jpg')">
+						<img :src="require('~/assets/images_aj/event2/ico/ev2_ico_banner.png')"
+							 class='ico-banner' />
+						<btn class='btn-box'
+							 btn-type='line-type'
+							 colorType='color-pink-white'
+							 width='235px'
+							 left='370px'
+							 bottom='16px'
+						>투표 현황 보러가기
+						</btn>
+					</event-section>
+				</tab-item>
+			</template>
+		</tab-items>
 	</div>
 </template>
 
 <script lang='ts'>
-import Vue from 'vue';
-import Component from "vue-class-component";
+import Vue from 'vue'
+// 클래스 기반의 API를 사용하는 경우 공식 vue-class-component 데코레이터를 사용할 수 있음
+// import Component from 'vue-class-component'
 
-// @Component 데코레이터는 클래스가 Vue 컴포넌트임을 나타냅니다
-@Component
+// vue-property-decorator는 vue-class-component를 기반으로 제작
+// vuejs에서 typescript로 개발할 때, 클래스 컴포넌트 스타일의 개발을 도와주는 데코레이터들을 제공해주는 모듈
+// @Component, @Prop, @Watch 등이 있음
+import { Component } from 'vue-property-decorator'
+
+export interface event1Data {
+	type: string,
+	name: string,
+	phone: string,
+	email: string,
+	certFile: object,
+	aggrchk1: boolean,
+	aggrchk2: boolean
+}
+
+/*
+* @Component
+* vuejs는 객체만을 처리
+* Class를 객체로 만들어서 vuejs가 처리할 수 있도록 해줌
+* */
+@Component({
+	layout: 'event'
+})
 export default class Index extends Vue {
-	activeTab: number = 1
-	activeTab2: string = 'event1'
 
-	tabItems = [
+	activeTab: number = 0
+	tabItems: object[] = [
 		{
 			name: 'event1',
+			key: 0,
+			title: '',
+			titlePoint: 'event1.',
+			subTitle: '한우 먹고!',
+			subTitlePoint: '한우 인증 GO~!'
 		},
 		{
 			name: 'event2',
+			key: 1,
+			title: '',
+			titlePoint: 'event2.',
+			subTitle: '최애 한우인증점 Pick!',
+			subTitlePoint: ''
 		}
 	]
 
-	event1TabImg: string = require('~/assets/images_aj/event1/ev1_tmp1.jpg')
-	event1Img1: string = require('~/assets/images_aj/event1/ev1_kv.jpg')
-	event1Img2: string = require('~/assets/images_aj/event1/ev1_step.jpg')
-	event1Img3: string = require('~/assets/images_aj/event1/ev1_tmp_banner.jpg')
-	event1Img4: string = require('~/assets/images_aj/event1/ev1_prize.jpg')
-	event1Img5: string = require('~/assets/images_aj/event1/ev1_join.jpg')
-	event1FootImg = require('~/assets/images_aj/event1/ev1_ft.jpg')
+	evStep01On: boolean = false
+	evStep02On: boolean = false
+	evStep03On: boolean = false
+	applyType: string = 'public'
 
-	event2TabImg: string = require('~/assets/images_aj/event2/ev2_tmp2.jpg')
-	event2Img1: string = require('~/assets/images_aj/event2/ev2_kv.jpg')
-	event2Img2: string = require('~/assets/images_aj/event2/ev2_step.jpg')
-	event2Img3: string = require('~/assets/images_aj/event2/ev2_tmp1.jpg')
-	event2Img33: string = require('~/assets/images_aj/event2/ev2_tmp3.jpg')
-	event2Img4: string = require('~/assets/images_aj/event2/ev2_tmp_banner.jpg')
-	event2FootImg = require('~/assets/images_aj/event2/ev2_ft.jpg')
+	event1Data: event1Data = {
+		type: '',
+		name: '',
+		phone: '',
+		email: '',
+		certFile: {},
+		aggrchk1: false,
+		aggrchk2: false
+	}
 
-	changeActiveTab(value: number) {
-		console.log(value)
+	filename: string = ''
+
+	areaActiveTab: number = 1
+	areaTabItem: object[] = [
+		{
+			name: 'area1',
+			key: 1,
+			title: '전국',
+			contents: [{ name: 'sec1' }]
+		},
+		{
+			name: 'area2',
+			key: 2,
+			title: '수도권',
+			contents: [{ name: 'sec1' }]
+		},
+		{
+			name: 'area3',
+			key: 3,
+			title: '충청도',
+			contents: [{ name: 'sec1' }]
+		},
+		{
+			name: 'area4',
+			key: 4,
+			title: '전라도',
+			contents: [{ name: 'sec1' }]
+		},
+		{
+			name: 'area5',
+			key: 5,
+			title: '경상도',
+			contents: [{ name: 'sec1' }]
+		},
+		{
+			name: 'area6',
+			key: 6,
+			title: '강원도',
+			contents: [{ name: 'sec1' }]
+		},
+		{
+			name: 'area7',
+			key: 7,
+			title: '제주도',
+			contents: [{ name: 'sec1' }]
+		}
+	]
+
+	storeList: object[] = [
+		{
+			index: 1,
+			title: '늘푸름 홍천 한우',
+			img: require('~/assets/images_aj/event2/store/store_thum02.jpg')
+		},
+		{
+			index: 2,
+			title: '세종 한우',
+			img: require('~/assets/images_aj/event2/store/store_thum02.jpg')
+		},
+		{
+			index: 3,
+			title: '모심정',
+			img: require('~/assets/images_aj/event2/store/store_thum03.png')
+		},
+		{
+			index: 4,
+			title: '농업회사법인초원(주)안양지점',
+			img: require('~/assets/images_aj/event2/store/store_thum04.png')
+		},
+		{
+			index: 5,
+			title: '다한우',
+			img: require('~/assets/images_aj/event2/store/store_thum04.png')
+		},
+		{
+			index: 6,
+			title: '끼리 한우',
+			img: require('~/assets/images_aj/event2/store/store_thum04.png')
+		},
+		{
+			index: 7,
+			title: '농업회사법인(주)포천한우백년',
+			img: require('~/assets/images_aj/event2/store/store_thum07.png')
+		},
+		{
+			index: 8,
+			title: '강화섬약쑥한우',
+			img: require('~/assets/images_aj/event2/store/store_thum08.png')
+		},
+		{
+			index: 9,
+			title: '늘푸름 홍천 한우',
+			img: require('~/assets/images_aj/event2/store/store_thum09.jpg')
+		}
+	]
+
+	changeMainTab(value: number) {
+		console.log('ttt')
 		this.activeTab = value
+	}
+
+	changeAreaTab(value: number) {
+		this.areaActiveTab = value
+	}
+
+	closeModal() {
+		this.event1Data = {
+			type: '',
+			name: '',
+			phone: '',
+			email: '',
+			certFile: [],
+			aggrchk1: false,
+			aggrchk2: false
+		}
+		this.filename = ''
+
+		this.evStep01On = false
+		this.evStep02On = false
+		this.evStep03On = false
+	}
+
+	selectApplyType(type: string) {
+		this.event1Data.type = type
+
+		this.evStep01On = false
+		this.evStep02On = true
+		this.evStep03On = false
+	}
+
+	event01Submit() {
+		console.log(this.event1Data)
+
+		this.closeModal()
+	}
+
+	setUploadFile(files: any) {
+		if (!files.length) {
+			this.filename = ''
+		} else {
+			this.filename = files[0].name
+		}
+		this.event1Data.certFile = files
 	}
 }
 </script>
 
-<style scoped>
+<style lang='scss' scoped>
 .inner {
 	max-width: 1024px;
 	margin: 0 auto;
-}
-.banner-img {
 
+	.list-wrap {
+		padding: 0 55px 60px 55px;
+	}
 }
+
 </style>
